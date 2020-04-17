6a8be6a5f63988df4eebbe17df87218a73fab8fe
Luis VR <luis_vazquez.r@hotmail.com>
2020-04-16 21:51:38 -0500

    Initial Commit

---
+++ contest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  contest
+//
+//  Created by LKY on 16/04/20.
+//  Copyright © 2020 LKY. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
