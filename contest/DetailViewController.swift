eab673d6c23ed37e511bf22cdd54e67fe8caf02d
Luis VR <luis_vazquez.r@hotmail.com>
2020-04-24 16:14:34 -0500

    ajustes en imagen de usuario

--- contest/DetailViewController.swift
+++ contest/DetailViewController.swift
@@ -9,12 +9,14 @@
 import UIKit
 
 class DetailViewController: UIViewController {
-    
+
+//se declaran los elementos de la vista
     @IBOutlet weak var imageView: UIImageView!
     @IBOutlet weak var nameLabel: UILabel!
     @IBOutlet weak var lastnameLabel: UILabel!
     @IBOutlet weak var emailLabel: UILabel!
     
+//se asigna un tipado a las variables
     var avatar = ""
     var first_name = ""
     var last_name = ""
@@ -23,11 +25,14 @@ class DetailViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+//se llama la funcion de la extension para convertir la url de la imagen
         guard let imageUrl:URL = URL(string: avatar) else {
             return
         }
         
+//se establecen los valores a los elementos
+        imageView.layer.cornerRadius = imageView.bounds.height / 2
         imageView.loadImge(withUrl: imageUrl)
         nameLabel.text = first_name
         lastnameLabel.text = last_name
@@ -37,6 +42,7 @@ class DetailViewController: UIViewController {
 
 }
 
+//extension que funciòn que convierte la url de la imagen en string
 extension UIImageView {
     func loadImge(withUrl url: URL) {
        DispatchQueue.global().async { [weak self] in
